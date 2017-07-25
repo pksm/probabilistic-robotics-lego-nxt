89ce506ca8258c6e5cda3c6ad682a1be9f2d19a6
pksm <paulamoraes95@gmail.com>
2017-07-25 17:05:38 -0300

    Update difPilot.java

--- difPilot.java
+++ difPilot.java
@@ -8,13 +8,13 @@ public class dPilot {
 	public static void main(String[] args) {
 		LCD.drawString("D.P. - SQUARE", 0, 0);
 		Button.waitForAnyPress();
-		DifferentialPilot pilot = new DifferentialPilot(2.205f, 4.527f, Motor.B, Motor.C, false);
+		DifferentialPilot pilot = new DifferentialPilot(2.205f, 4.527f, Motor.B, Motor.C, false); //in inches
 		LCD.clear();
 		pilot.setRotateSpeed(18);
 		pilot.setTravelSpeed(4);
 		for (int i = 0; i < 4; i++) {
 			pilot.travel(15.75, false); //approx. 40cm
-			pilot.rotate(90, false);
+			pilot.rotate(90, false); //90 degrees
 		}
 		pilot.stop();
 
