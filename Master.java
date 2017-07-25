55a4f078a47b8e3e424a2f3702f35dc28b0cba9c
pksm <paulamoraes95@gmail.com>
2017-07-25 17:25:00 -0300

    Add files via upload

---
+++ Master.java
@@ -0,0 +1,105 @@
+import lejos.pc.comm.*;
+import java.util.Scanner;
+import java.io.*;
+/**
+ * Master: Sends commands to NXT Slave application
+ * @author Denis Maua'
+ * @since 2016-06-05
+ *
+ */
+public class Master {
+	private static final byte ROTATE = 0;
+	private static final byte ROTATETO = 1;
+	private static final byte SQUARE = 2;
+	private static final byte STOP = 3;
+	
+	private NXTComm nxtComm;
+	private DataOutputStream dos;
+	private DataInputStream dis;	
+	// NXT BRICK ID
+	private static final String NXT_ID = "NXT8";	
+	
+	/**
+	 * Send command to the robot
+	 * @param command specifies command
+	 * @param param argument
+	 * @return value returned by the robot (float)
+	 */
+	private float sendCommand(byte command, float param) {
+		try {
+			dos.writeByte(command);
+			dos.writeFloat(param);
+			dos.flush();
+			return dis.readFloat();
+		} catch (IOException ioe) {
+			System.err.println("IO Exception");
+			System.exit(1);
+			return -1f;
+		}
+	}
+	
+	/**
+	 * Connect to the NXT
+	 *
+	 */
+	private void connect() {
+		try {
+			NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.USB);
+			/* Uncomment next line for Blluetooth communication */
+			//NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.BLUETOOTH);			
+			NXTInfo[] nxtInfo = nxtComm.search(Master.NXT_ID);
+			
+			if (nxtInfo.length == 0) {
+				System.err.println("NO NXT found");
+				System.exit(1);
+			}
+			
+			if (!nxtComm.open(nxtInfo[0])) {
+				System.err.println("Failed to open NXT");
+				System.exit(1);
+			}
+			
+			dis = new DataInputStream(nxtComm.getInputStream());
+			dos = new DataOutputStream(nxtComm.getOutputStream());
+			
+		} catch (NXTCommException e) {
+			System.err.println("NXTComm Exception: "  + e.getMessage());
+			System.exit(1);
+		}
+	}		
+	/**
+	 * Terminate the program and send stop command to the robot
+	 *
+	 */
+	private void close() {
+		try {
+			dos.writeByte(STOP);
+			dos.writeFloat(0f);
+			dos.flush();
+			Thread.sleep(200);
+			System.exit(0);
+		} catch (Exception ioe) {
+			System.err.println("IO Exception");
+		}
+	}	
+	
+	public static void main(String[] args) {
+		byte cmd = 0; float param = 0f; float ret=0f; 
+		Master master = new Master();
+		master.connect();
+	    Scanner scan = new Scanner( System.in );	    
+	    while(true) {
+	    	System.out.print("Enter command [0:ROTATE 1:ROTATETO 2:SQUARE 3:STOP]: ");
+	    	cmd = (byte) scan.nextFloat(); 
+	    	if (cmd < 2) {
+	    	 System.out.print("Enter param [float]: ");
+	    	 param = scan.nextFloat();
+	    	} else {
+	    		param = 0;	    		
+	    	}
+	    	ret = master.sendCommand(cmd, param);
+	    	System.out.println("cmd: " + cmd + " param: " + param + " return: " + ret);
+	    }
+	}
+
+}
---
+++ Slave.java
@@ -0,0 +1,75 @@
+import lejos.nxt.*;
+import lejos.nxt.comm.*;
+import lejos.nxt.Motor;
+import java.io.*;
+
+/**
+ * Slave: Executes commands sent by PC Master application
+ * @author Denis Maua'
+ * @since 2016-06-05
+ * Inspired on code by Lawrie Griffiths
+ *
+ */
+
+public class Slave {
+	private static final byte ROTATE = 0;
+	private static final byte ROTATETO = 1;
+	private static final byte SQUARE = 2;
+	private static final byte STOP = 3;
+
+	public static void main(String[] args) throws Exception {
+		//USBConnection btc = USB.waitForConnection(); /* USB communication */
+		/* Uncomment next line for Bluetooth */
+		BTConnection btc = Bluetooth.waitForConnection();
+		DataInputStream dis = btc.openDataInputStream();
+		DataOutputStream dos = btc.openDataOutputStream();
+		UltrasonicSensor sonar = new UltrasonicSensor(SensorPort.S1);
+		
+		Motor.C.setSpeed(50);
+		LCD.drawString("READY", 0, 10);
+		while (true) {
+			try {
+				byte cmd = dis.readByte();
+				LCD.drawInt(cmd,1,0,0);
+				float param = dis.readFloat();
+				LCD.drawInt((int) (param + 0.5f),4,0,1);
+				
+				switch (cmd) {
+				case ROTATE: 
+					Motor.C.rotate((int) (param + 0.5f));
+					dos.writeFloat(0);
+					break;
+				case ROTATETO: 
+					Motor.C.rotateTo((int) (param + 0.5f));
+					dos.writeFloat(0);
+					break;
+				case SQUARE:
+					Square();
+					dos.writeFloat(0);
+					break;				
+				case STOP:
+					System.exit(1);
+				default:
+					dos.writeFloat(-1);
+				}
+				dos.flush();
+				
+			} catch (IOException ioe) {
+				System.err.println("IO Exception");
+				Thread.sleep(2000);
+				System.exit(1);
+			}
+		}
+	}
+	public static void Square(){
+		DifferentialPilot pilot = new DifferentialPilot(2.205f, 4.527f, Motor.B, Motor.C, false); //in inches
+		pilot.setRotateSpeed(18);
+		pilot.setTravelSpeed(4);
+		for (int i = 0; i < 4; i++) {
+			pilot.travel(15.75, false); //approx. 40cm
+			pilot.rotate(90, false); //90 degrees
+		}
+		pilot.stop();
+	}
+
+}
