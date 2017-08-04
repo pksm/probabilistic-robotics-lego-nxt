72c4dbee605814f0a9c536e3508055c99580740c
pksm <paulamoraes95@gmail.com>
2017-08-04 16:33:49 -0300

    código comentado

--- MasterSlave/MasterSquare.java
+++ MasterSlave/MasterSquare.java
@@ -12,11 +12,11 @@ public class Master {
 	private static final byte BATTERY = 1;
 	private static final byte STOP = 2;
 	
-	private NXTComm nxtComm;
-	private DataOutputStream dos;
-	private DataInputStream dis;	
+	private NXTComm nxtComm; //NXTComm - PC initiator to connect to NXT
+	private DataOutputStream dos; // outout data
+	private DataInputStream dis; // input data
 	
-	private static final String NXT_ID = "NXT8"; // NXT BRICK ID
+	private static final String NXT_ID = "NXT8"; // NXT BRICK NAME
 	
 	/**
 	 * Send command to the robot
@@ -24,13 +24,13 @@ public class Master {
 	 * @param param argument
 	 * @return value returned by the robot (float)
 	 */
-	private float sendCommand(byte command, float param) {
+	private float sendCommand(byte command, float param) { // try sending byte commands to brick and returns a float ouputted by Slave
 		try {
-			dos.writeByte(command);
-			dos.writeFloat(param);
-			dos.flush();
-			return dis.readFloat();
-		} catch (IOException ioe) {
+			dos.writeByte(command); // send byte with desired command
+			dos.writeFloat(param); // send float with required parameter
+			dos.flush(); // flush the output stream to be sure the data is actually transmitted
+			return dis.readFloat(); // return float sent by Slave
+		} catch (IOException ioe) { // exception handler
 			System.err.println("IO Exception");
 			System.exit(1);
 			return -1f;
@@ -39,14 +39,13 @@ public class Master {
 	
 	/**
 	 * Connect to the NXT
-	 *
 	 */
 	private void connect() {
 		try {
-			NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.USB);
+			NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.USB); //USB initiator
 			/* Uncomment next line for Bluetooth communication */
-			//NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.BLUETOOTH);			
-			NXTInfo[] nxtInfo = nxtComm.search(Master.NXT_ID);
+			//NXTComm nxtComm = NXTCommFactory.createNXTComm(NXTCommFactory.BLUETOOTH); // Bluetooth initiator		
+			NXTInfo[] nxtInfo = nxtComm.search(Master.NXT_ID); //find the available bricks with this NXT_ID by doing a Bluetooth inquiry
 			
 			if (nxtInfo.length == 0) {
 				System.err.println("NO NXT found");
@@ -58,17 +57,16 @@ public class Master {
 				System.exit(1);
 			}
 			
-			dis = new DataInputStream(nxtComm.getInputStream());
-			dos = new DataOutputStream(nxtComm.getOutputStream());
+			dis = new DataInputStream(nxtComm.getInputStream()); // open data input stream 
+			dos = new DataOutputStream(nxtComm.getOutputStream()); // open data output stream
 			
-		} catch (NXTCommException e) {
+		} catch (NXTCommException e) { // exception handler
 			System.err.println("NXTComm Exception: "  + e.getMessage());
 			System.exit(1);
 		}
 	}		
 	/**
 	 * Terminate the program and send stop command to the robot
-	 *
 	 */
 	private void close() {
 		try {
@@ -86,18 +84,18 @@ public class Master {
 		byte cmd = 0; float param = 0f; float ret=0f; 
 		Master master = new Master();
 		master.connect();
-	    Scanner scan = new Scanner( System.in );	    
+	    Scanner scan = new Scanner( System.in ); // create object to read from keyboard	    
 	    while(true) {
-	    	System.out.print("Enter command [0:ROTATE 1:ROTATETO 2:SQUARE 3:STOP]: ");
-	    	cmd = (byte) scan.nextFloat(); 
-	    	if (cmd < 2) {
-	    	 System.out.print("Enter param [float]: ");
+	    	System.out.print("Enter command [0:Square 1:Battery Level 2:Stop]: ");
+	    	cmd = (byte) scan.nextFloat(); // read float and converts it to byte
+	    	if (cmd == 0) { 
+	    	 System.out.print("Enter square size in cm [float]: ");
 	    	 param = scan.nextFloat();
 	    	} else {
 	    		param = 0;	    		
 	    	}
 	    	ret = master.sendCommand(cmd, param);
-	    	System.out.println("cmd: " + cmd + " param: " + param + " return: " + ret);
+	    	System.out.println("Command: " + cmd + " param: " + param + " return: " + ret); // print 
 	    }
 	}
 
