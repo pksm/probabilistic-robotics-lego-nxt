3524179fde0884e08e864fdaaca0a52e29b256b8
pksm <paulamoraes95@gmail.com>
2017-08-24 20:39:36 -0300

    Create SensorIR.java

---
+++ LineFollower/SensorIR.java
@@ -0,0 +1,44 @@
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.LightSensor;
+import lejos.nxt.MotorPort;
+import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.UltrasonicSensor;
+import lejos.nxt.*;
+import lejos.util.Stopwatch;
+ 
+public class SensorIR {
+        public static void main(String [] args){
+        	int Kp = 240;
+        	int Ki = 0;
+        	int Kd = 25;
+        	int white = 54;
+        	int black = 32;
+        	int offset = (white+black)/2;
+        	int lastError = 0;
+        	int derivative = 0;
+        	int Tp = 25;
+        	int integral = 0;
+        	LightSensor light = new LightSensor(SensorPort.S4);
+        	NXTMotor mB = new NXTMotor(MotorPort.B);
+        	NXTMotor mC = new NXTMotor(MotorPort.C);
+        	Stopwatch s1 = new Stopwatch();
+        	while(true){
+        		LCD.drawInt(light.getLightValue(), 4, 0, 0);
+        		int Lval = light.getLightValue();
+        		int error = Lval - offset;
+        		derivative = error - lastError;
+        		integral += error;
+        		int turn = (Kp*error) + (Ki*integral) + (Kd*derivative);
+        		turn /= 100;
+        		int powB = Tp-turn;
+        		int powC = Tp + turn;
+        		mB.setPower(powB);
+        		mC.setPower(powC);
+        		lastError = error;
+        	}
+        }
+}
+
+
