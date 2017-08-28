c6a597d297ae4f479b3903999728ce1fbd74a4b2
pksm <paulamoraes95@gmail.com>
2017-08-28 12:55:17 -0300

    Create unregulatedMotor.java

---
+++ LineFollower/unregulatedMotor.java
@@ -0,0 +1,29 @@
+import lejos.nxt.Button;
+import lejos.nxt.LightSensor;
+import lejos.nxt.MotorPort;
+import lejos.nxt.NXTMotor;
+import lejos.nxt.SensorPort;
+
+public class unregulatedMotor {
+	
+	static LightSensor light;
+	static NXTMotor mB;
+	static NXTMotor mC;
+	
+	public static void main(String args[])  
+    {
+		light = new LightSensor(SensorPort.S4);
+		mB = new NXTMotor(MotorPort.B);
+		mC = new NXTMotor(MotorPort.C);
+		Button.waitForAnyPress();
+		while (light.getLightValue() < 50){
+			mB.setPower(50);
+			mC.setPower(50);
+			//mB.setPower(-50);
+			//mC.setPower(-50);
+		}
+		mB.stop();
+		mC.stop();
+    }
+		
+}
