e80e57e506f57d1aebf642d610493efa214ee25d
pksm <paulamoraes95@gmail.com>
2017-09-02 04:04:02 -0300

    Update MotorTypes.java

--- LineFollower/MotorTypes.java
+++ LineFollower/MotorTypes.java
@@ -1,3 +1,8 @@
+/*
+Example of both regulated motor and unregulated motor operating in the same code.
+
+This program is intended to run as a NXT Application (compile and run for NXT brick)
+*/
 import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.MotorPort;
 import lejos.nxt.NXTMotor;
@@ -7,38 +12,35 @@ import lejos.nxt.Motor;
 
 public class MotorTypes {
 
-	
 	public static void main(String args[])  
   	{
-		NXTMotor mB = new NXTMotor(MotorPort.B);
-		NXTMotor mC = new NXTMotor(MotorPort.C);
-		NXTRegulatedMotor rb = new NXTRegulatedMotor(MotorPort.B);
-		NXTRegulatedMotor rc = new NXTRegulatedMotor(MotorPort.C);
+		NXTMotor mB = new NXTMotor(MotorPort.B); //create instance of an unregulated motor in motor port B 
+		NXTMotor mC = new NXTMotor(MotorPort.C); //create instance of an unregulated motor in motor port C 
+		NXTRegulatedMotor rb = new NXTRegulatedMotor(MotorPort.B); //create instance of a regulated motor in motor port B 
+		NXTRegulatedMotor rc = new NXTRegulatedMotor(MotorPort.C); //create instance of a regulated motor in motor port C
 		
-		boolean bola, bolc;		
-
+		boolean bola, bolc; //probably not necessary ...NEED TO REFACTOR		
+		//UNREGULATED
 		Button.waitForAnyPress();
-    		mB.setPower(50);
+    		mB.setPower(50); 
 		mC.setPower(50);
     		while (!Button.ESCAPE.isDown());
 		mB.stop();
 		mC.stop();
 
-
+		//REGULATED
+		//one can go from uregulated to regulated automatically using a NXTRegulataedMotor method
    		rb.rotate(3000,true);
 		rc.rotate(-3000,true);
 		Delay.msDelay(2000);
-		
-
 
 		rb.stop(true);
 		rc.stop();
-		
-
-		
-		bola = rb.suspendRegulation();
+		//regulated to unregulated
+		bola = rb.suspendRegulation(); 
 		bolc = rc.suspendRegulation();
-
+		
+		//UNREGULATED
 		System.out.println(""+bola);	
 		System.out.println(""+bolc);
 		mB.setPower(-30);
@@ -48,6 +50,8 @@ public class MotorTypes {
 		System.out.println(""+bola+" "+mB.getPower());	
 		System.out.println(""+bolc+" "+mC.getPower());	
 		Delay.msDelay(800);
+		
+		//REGULATED
 		rb.stop(true);
 		rc.stop();
 		rb.rotate(300,true);
