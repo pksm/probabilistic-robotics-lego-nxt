159582d424dec7a1848e57c5cb569738d6cad1ce
pksm <paulamoraes95@gmail.com>
2017-09-11 13:03:50 -0300

    Update SlaveNav.java

--- Maps/SlaveNav.java
+++ Maps/SlaveNav.java
@@ -8,7 +8,7 @@ import lejos.robotics.navigation.Waypoint;
 import lejos.robotics.localization.OdometryPoseProvider;
 import lejos.robotics.navigation.Pose;
 
-public class Slave {
+public class SlaveNav {
 	private static final byte ADD_POINT = 0; //adds waypoint to path
 	private static final byte TRAVEL_PATH = 1; // enables slave to execute the path
 	private static final byte STATUS = 2; // enquires about slave's position 
