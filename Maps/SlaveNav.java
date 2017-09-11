39b70afbd673d17f5a92db4a9d7b0c7bab94b622
pksm <paulamoraes95@gmail.com>
2017-09-11 15:36:00 -0300

    class september 11

--- Maps/SlaveNav.java
+++ Maps/SlaveNav.java
@@ -48,7 +48,8 @@ public class SlaveNav {
 					dos.writeBoolean(nav.pathCompleted()); // Returns true if the the final waypoint has been reached
 					break;	
 				case SET_START:
-					position.setPose(addX,addY);
+					Pose init = new Pose(addX,addY);
+					position.setPose(init);
 					dos.writeFloat(0);
 					break;
 				case STOP:
