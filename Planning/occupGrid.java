cdf63a2681cf2f4e0f4af7f0c4f4f4a1a0116fe2
pksm <paulamoraes95@gmail.com>
2017-09-25 13:30:04 -0300

    2D matrix as a binary image

--- Planning/occupGrid.java
+++ Planning/occupGrid.java
@@ -14,47 +14,74 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.List;
 import java.awt.Point;
-//import java.util.PriorityQueue; 
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class occupGrid{
   
   public static int sizeX;
   public static int sizeY;
+  public int width;
+  public int height;
+  public int cellSize;
+  public int[][] map;
   public static ArrayList<Point> actions; 
 
-  public static int[][] mapToGrid(int w, int h, int cellSize){ //width and height
-  		sizeX = (int) Math.ceil((float)w / cellSize);
-  		sizeY =(int) Math.ceil((float)h / cellSize);
-  		int [][] map = new int [sizeX][sizeY];
-  		for (int row = 0; row < sizeX; row++ )
-     		for (int column = 0; column < sizeY; column++)
+  public occupGrid(int w, int h, int cs)
+	{
+		this.width = w;
+		this.height = h;
+		this.cellSize = cs;
+		this.sizeX = (int)Math.ceil((float)w/cs);
+		this.sizeY = (int)Math.ceil((float)h/cs);
+  		this.map = new int[this.sizeX][this.sizeY];
+	}
+
+  private void setActions(int type)
+	{
+		// 4-Neighborhood
+		this.actions = new ArrayList<Point>();
+		this.actions.add(new Point(-1,0)); // North
+		this.actions.add(new Point(1,0)); // South
+		this.actions.add(new Point(0,1)); // East
+		this.actions.add(new Point(0,-1)); // West
+
+		// 8-Neighborhood
+		if(type == 8)
+		{
+			this.actions.add(new Point(-1,-1)); // NW
+			this.actions.add(new Point(-1,1)); // NE
+			this.actions.add(new Point(1,-1)); // SW
+			this.actions.add(new Point(1,1)); // SE
+		}
+	}
+
+  private void buildGrid(){ 
+  		for (int row = 0; row < this.sizeX; row++ )
+     		for (int column = 0; column < this.sizeY; column++)
      		{
-        		map[row][column] = 999; //initialize with 999
+        		this.map[row][column] = 999; //initialize with 999
     		}
-    	return map;
   }
 
-  public static void printGrid(int[][] map, int w, int h, int cellSize){
-  	//int sizeX = (int) Math.ceil((float)w / cellSize);
-  	//int sizeY =(int) Math.ceil((float)h / cellSize);
-  	for (int row = 0; row < sizeX; row++ ){
-		for (int column = 0; column < sizeY; column++)
+  private void printGrid(){
+  	for (int row = 0; row < this.sizeX; row++ ){
+		for (int column = 0; column < this.sizeY; column++)
 		{
-			System.out.print(" "+ map[row][column]);
+			System.out.print(" "+ this.map[row][column]);
 		}
 		System.out.println("");
 	}
   }
-  public static Set<Point> reachableCells(int[][] map, Point p){ // 4- neighboor  
+  private Set<Point> reachableCells(Point p){ // 4- neighboor  
   	Point cell = new Point(p);
   	Set<Point> neighboors = new HashSet<Point>();
   	for (int i = 0; i < actions.size(); i++) {
-		if ((((cell.x + actions.get(i).x) >= 0) && ((cell.y + actions.get(i).y) >=0)) && ((cell.x + actions.get(i).x) < sizeX) && ((cell.y + actions.get(i).y) < sizeY)){
-			if (map[(cell.x + actions.get(i).x)][(cell.y + actions.get(i).y)] == -1)
+		if ((((cell.x + actions.get(i).x) >= 0) && ((cell.y + actions.get(i).y) >=0)) && ((cell.x + actions.get(i).x) < this.sizeX) && ((cell.y + actions.get(i).y) < this.sizeY)){
+			if (this.map[(cell.x + actions.get(i).x)][(cell.y + actions.get(i).y)] == -1)
 				continue; 
 			cell.translate(actions.get(i).x, actions.get(i).y);
 			neighboors.add(cell);
@@ -66,11 +93,10 @@ public class occupGrid{
   	//System.out.println("Actions: "+ Arrays.toString(actions.toArray())); print actions
   }
 
-  public static List<Node> bfs(int[][] grid, int gx, int gy, int sx, int sy){ //gx, gy - goal coordinates --- sx, sy - start coordinates
+  private List<Node> bfs(int gx, int gy, int sx, int sy){ //gx, gy - goal coordinates --- sx, sy - start coordinates
   	Set<Point> explored = new HashSet<Point>(); //creates explored set of points
   	Set<Point> inQueue = new HashSet<Point>(); //create inQueue set of points that are in the queue but were not explored
   	Set<Point> nextCell;
-  	//PriorityQueue<Node> fringe = new PriorityQueue<Node>(20); // creates queue of nodes
   	Queue<Node> fringe = new LinkedList<Node>();
   	Point goal = new Point(gx, gy);
   	Point init = new Point(sx,sy);
@@ -79,17 +105,17 @@ public class occupGrid{
   	boolean foundInit = false;
   	fringe.add(nodeParent);
   	inQueue.add(goal);
-  	alterGrid(grid, goal, 0);
+  	alterGrid(goal, 0);
   	while (!foundInit && !fringe.isEmpty()){
   		nodeParent = fringe.poll();
   		inQueue.remove(nodeParent.getState());
   		explored.add(nodeParent.getState());
-  		alterGrid(grid,nodeParent.getState(),nodeParent.getPathCost());
+  		alterGrid(nodeParent.getState(),nodeParent.getPathCost());
   		if (init.equals(nodeParent.getState())){
   			foundInit = true;
   			break;
   		}
-  		nextCell = reachableCells(grid,nodeParent.getState());
+  		nextCell = reachableCells(nodeParent.getState());
   		for (Point p : nextCell) {
 			if (!explored.contains(p) && !inQueue.contains(p)){
 				nodeSon = new Node(p,nodeParent,1);
@@ -107,42 +133,35 @@ public class occupGrid{
 	Any operations that alterGrid performs on mapRef are such that, for all practical purposes, they are performed on map itself 
 	(except when mapRef is changed to point to a different int[][] instance like mapRef = new int [sizeX][sizeY]) 
  */
-  public static void alterGrid(int [][] mapRef, int i, int j, int val){
-  		mapRef[i][j] = val;
+  private void alterGrid(int i, int j, int val){
+  		this.map[i][j] = val;
   }
-  public static void alterGrid(int [][] mapRef, Point p, int val){
-  		mapRef[p.x][p.y] = val;
+  private void alterGrid(Point p, int val){
+  		this.map[p.x][p.y] = val;
   }
 
 
   public static void main(String[] args){
-  	actions = new ArrayList<Point>(); //4 - neighboor
   	List<Node> nodePath;
- 	Point act = new Point(-1,0); // North
-  	actions.add(act);
-  	act = new Point(1,0); // South
-  	actions.add(act);
-  	act = new Point(0,1); // East
-  	actions.add(act);
-  	act = new Point(0,-1); // West
-  	actions.add(act);
-  	int [][] map = mapToGrid(10,16,2);
+	occupGrid oc = new occupGrid(10,16,2);
+	oc.buildGrid();
+	oc.setActions(4);
   	Point obs = new Point(); 
   	//setting obstacles
-  	alterGrid(map,1,2,-1);
-  	alterGrid(map,2,2,-1);
-  	alterGrid(map,3,2,-1);
-  	alterGrid(map,3,3,-1);
+  	oc.alterGrid(1,2,-1);
+  	oc.alterGrid(2,2,-1);
+  	oc.alterGrid(3,2,-1);
+  	oc.alterGrid(3,3,-1);
   	//exemplo de como usar a classe Point
   	obs.setLocation(2,5); 
-  	alterGrid(map,obs.getLocation(),-1);
+  	oc.alterGrid(obs.getLocation(),-1);
   	//
-  	alterGrid(map,1,5,-1);
-  	alterGrid(map,1,6,-1);
-  	alterGrid(map,2,6,-1);
+  	oc.alterGrid(1,5,-1);
+  	oc.alterGrid(1,6,-1);
+  	oc.alterGrid(2,6,-1);
   	//
-  	nodePath = bfs(map,4,7,2,3);
-  	printGrid(map,10,16,2);
+  	nodePath = oc.bfs(4,7,2,3);
+  	oc.printGrid();
   	Collections.reverse(nodePath);
   	for (int i = 0; i < nodePath.size(); i++) 
   		System.out.println(""+nodePath.get(i).getState());
